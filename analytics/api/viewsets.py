271bc235bb1b2b4095596fa3a141280bf91f7ac3
Насибуллин Оскар Рамилевич <oscarn08@gmail.com>
2018-09-29 19:49:02 +0300

    importer

--- analytics/api/viewsets.py
+++ analytics/api/viewsets.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from django.shortcuts import get_object_or_404
-from analytics.models import Tag
+from analytics.models import Tag, Question
 from answers_mail.permissions import permissions, ReadOnly
-from serializers import TagSerializer
+from serializers import TagSerializer, QuestionSerializer
 
 
 class TagViewSet(ReadOnlyModelViewSet):
@@ -13,6 +13,6 @@ class TagViewSet(ReadOnlyModelViewSet):
 
 
 class QuestionsViewSet(ReadOnlyModelViewSet):
-    queryset = Tag.objects.all().order_by('-created_at')
-    serializer_class = TagSerializer
+    queryset = Question.objects.all().order_by('-created_at')
+    serializer_class = QuestionSerializer
     permission_classes = (ReadOnly, )
--- analytics/views.py
+++ analytics/views.py
@@ -3,27 +3,80 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-import datetime
+import datetime, time
 import requests
+from analytics.models import Question, Tag
 import json
 # Create your views here.
 
 
-def import_data(request, page_from = -1, page_to = -1):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now {}.</b> from: {} </b> to: {}</body></html>".format(now, page_from, page_to)
-    if (page_to == -1):
-        r = requests.get('https://otvet.mail.ru/api/v2/question?qid={}'.format(page_from))
-        if (r.status_code == 200):
-            # data = json.loads('{"lat":444, "lon":555}')
-            html = "<html><body> {} </body></html>".format(r.text)
 
-            # r.headers['content-type']
-        # 'application/json; charset=utf8'
-        #  r.encoding
-        # 'utf-8'
-        # >> > r.text
-        # u'{"type":"User"...'
-        # >> > r.json()
-        # {u'private_gists': 419, u'total_private_repos': 77, ...}
+def roundTime(dt=None, roundTo=60):
+   """Round a datetime object to any time lapse in seconds
+   dt : datetime.datetime object, default now.
+   roundTo : Closest number of seconds to round to, default 1 minute.
+   Author: Thierry Husson 2012 - Use it as you want but don't blame me.
+   """
+   if dt == None : dt = datetime.datetime.now()
+   seconds = (dt.replace(tzinfo=None) - dt.min).seconds
+   rounding = (seconds+roundTo/2) // roundTo * roundTo
+   return dt + datetime.timedelta(0,rounding-seconds,-dt.microsecond)
+
+
+
+
+def import_one(page_from):
+    now = datetime.datetime.now()
+    r = requests.get('https://otvet.mail.ru/api/v2/question?qid={}'.format(page_from))
+    if (r.status_code == 200):
+        data = r.json()
+
+        question_text = data['description'] + ' ' + data['qtext']
+        bad_date = int(data['added'])
+        ok_date = roundTime(datetime.datetime.fromtimestamp(time.mktime(now.timetuple()) - bad_date), 60 * 60 * 24)
+        print('importing from {}'.format(page_from))
+        # ok_date = datetime.datetime.fromtimestamp(ok_date)
+
+        # tag_list = [s for s in data['category']['name'].split(',')]
+        tag_list = data['category']['name']
+        html = "<html><body> question:\t{}<br/>tags:\t{}<br/>seconds:{}\t</body></html>".format(question_text, tag_list,
+                                                                                                ok_date)
+        # html = "<html><body> {} ---- {} </body></html>".format(time.mktime(now.timetuple()), bad_date)
+
+        try:
+            go = Question.objects.get(text=question_text)
+            go.delete()
+            html = "<html><body> {} </body></html>".format('question already exists, deleting, retry')
+        except Question.DoesNotExist:
+            go = None
+            # html = "<html><body> {} </body></html>".format('not exist')
+
+            question = Question(text=question_text, created_at=ok_date.date())
+            question.save()
+
+            # for s_tag in tag_list:
+            s_tag = tag_list  # ETO VREMENNO
+            if Tag.objects.filter(text__exact=s_tag).count() == 0:
+                tag = Tag(text=s_tag)
+                tag.save()
+                tag.questions.add(Question.objects.get(id=question.id))
+                tag.save()
+
+    else:
+        html = "<html><body>FUCKED UP with connection to otveti</body></html>"
+    return html
+
+
+def import_data(request, page_from = -1, page_to = -1):
+    html = "<html><body>Importing:<br/>\tfrom: {} <br/> to: {} <br/> errors {}</body></html>".format(int(page_from), int(page_to), '{}')
+    if (page_to == -1):
+        HttpResponse(import_one(page_from))
+    elif page_from != -1 and page_to != -1:
+        for i in range(int(page_from), int(page_to)):
+            try:
+                import_one(i)
+            except Exception as e:
+                html.format('failed importing page {}'. format(str(i) + '{}'))
+                print('failed importing page {}'. format(i))
+                pass
     return HttpResponse(html)
