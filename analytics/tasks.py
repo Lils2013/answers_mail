7119cd84b05f1b1b92e8945370d3740b1f0e655f
Alexander Tsoy <Lils2010@gmail.com>
2018-11-03 14:45:12 +0300

    fixed import

--- analytics/tasks.py
+++ analytics/tasks.py
@@ -26,46 +26,46 @@ def import_new():
     import time
 
     lock_id = '{0}-lock'.format("import_new")
-    # with memcache_lock(lock_id, "import_new") as acquired:
-    #     if acquired:
+    with memcache_lock(lock_id, "import_new") as acquired:
+        if acquired:
             # start_id = 211130000
-    try:
-        start_id = Question.objects.latest('id').id
-    except Exception as e:
-        start_id = 210775613
-        pass
-    pages = 10000
-    start_id += pages + 1
-    print("starting new API import from id {}".format(start_id))
-    try:
-        for i in range(pages):
-            start_id -= 1
-            start = datetime.now()
             try:
-                data = get_api(start_id)
+                start_id = Question.objects.latest('id').id - 1
+            except Exception as e:
+                start_id = 210775613
+                pass
+            pages = 10000
+            # start_id += pages + 1
+            print("starting new API import from id {}".format(start_id))
+            try:
+                for i in range(pages):
+                    start_id += 1
+                    start = datetime.now()
+                    try:
+                        data = get_api(start_id)
+                    except Exception as e:
+                        print(e)
+                        continue
+                    if len(data) == 0:
+                        print("Вопросы кончились!")
+                        break
+                    end1 = datetime.now()
+                    print(end1 - start)
+                    parsed = parse_question(data)
+                    end2 = datetime.now()
+                    print(end2 - end1)
+                    save_question(parsed)
+                    end3 = datetime.now()
+                    print(end3 - end2)
+                    print("loaded page: {}".format(i - 1))
+                print('update_global_idf_start')
+                # update_global_idf()
+                print('update_local_idf_start')
+                # update_local_idf()
             except Exception as e:
                 print(e)
-                continue
-            if len(data) == 0:
-                print("Вопросы кончились!")
-                break
-            end1 = datetime.now()
-            print(end1 - start)
-            parsed = parse_question(data)
-            end2 = datetime.now()
-            print(end2 - end1)
-            save_question(parsed)
-            end3 = datetime.now()
-            print(end3 - end2)
-            print("loaded page: {}".format(i - 1))
-        print('update_global_idf_start')
-        # update_global_idf()
-        print('update_local_idf_start')
-        # update_local_idf()
-    except Exception as e:
-        print(e)
-    return
-    # print('Questions are already being imported by another worker')
+            return
+    print('Questions are already being imported by another worker')
 
 
 
